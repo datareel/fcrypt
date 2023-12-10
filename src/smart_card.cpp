d43793f4aeccaa0a2f6bf8414a8441fee1f38ae9
dgaer <dgaer@nco-lw-rhel7-default-gfe-build.ncep.noaa.gov>
2023-12-10 18:32:49 -0500

    updated from RHEL 7 testing

--- src/smart_card.cpp
+++ src/smart_card.cpp
@@ -40,6 +40,8 @@ Smartcard encryption and decryption routines.
 const char *SC_default_enginePath = "/usr/lib64/engines-1.1/pkcs11.so";
 #elif defined (__RHEL9__)
 const char *SC_default_enginePath = "/usr/lib64/engines-3/pkcs11.so";
+#elif defined (__RHEL7__)
+const char *SC_default_enginePath = "/usr/lib64/openssl/engines/pkcs11.so";
 #else
 const char *SC_default_enginePath = "/usr/lib64/engines/pkcs11.so";
 #endif
